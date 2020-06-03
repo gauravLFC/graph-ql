826d4a53467cd7b9aa04dfc5ffa5e4c83e3e0aa0
Gaurav Sharma <gauravlfc.93@gmail.com>
2020-06-03 11:16:29 +0530

    Init commit

---
+++ client/components/LoginForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class LoginForm extends React.Component {
+  render() {
+    return <div>Login</div>;
+  }
+}
+
+export default LoginForm;
--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: '/',
-    filename: 'bundle.js'
+    path: '/build',
+    filename: 'bundle1.js',
   },
   module: {
     rules: [
       {
         use: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'client/index.html'
-    })
-  ]
+      template: 'client/index.html',
+    }),
+  ],
 };
